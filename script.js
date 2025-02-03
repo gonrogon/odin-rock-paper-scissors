2de63e7d692365a6597685a7756c5d6e941a0095
gonrogon <gonrogon@gmail.com>
2025-02-03 20:43:28 +0100

    Add a function to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const ROCK     = "rock";
+const PAPER    = "paper";
+const SCISSORS = "scissors";
+
+function getComputerChoice()
+{
+    let random = Math.trunc(Math.random() * 3);
+
+    switch (random)
+    {
+        case 0:  return ROCK;
+        case 1:  return PAPER;
+        default: return SCISSORS;
+    }
+}
