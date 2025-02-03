ac8be65e4ba195642c80dd527d32f7793e2315a4
gonrogon <gonrogon@gmail.com>
2025-02-04 00:30:16 +0100

    Add a function to play a round

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const ROCK     = "rock";
 const PAPER    = "paper";
 const SCISSORS = "scissors";
 
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice()
 {
     let random = Math.trunc(Math.random() * 3);
@@ -32,3 +35,41 @@ function getHumanChoice()
 
     return choice;
 }
+
+function playRound(humanChoice, computerChoice)
+{
+    // Human choice validation and normalization (case-insensitive) is already done in
+    // the function "getHumanChoice".
+    switch (humanChoice)
+    {
+        case ROCK:
+            switch (computerChoice)
+            {
+                case ROCK:                      console.log("Tie!"); break;
+                case PAPER:    computerScore++; console.log("You lose! Paper beats Rock!"); break;
+                case SCISSORS: humanScore++;    console.log("You win! Rock beats Scissors!"); break;
+            }
+            break;
+        case PAPER:
+            switch (computerChoice)
+            {
+                case ROCK:     humanScore++;    console.log("You win! Paper beats Rock!"); break;
+                case PAPER:                     console.log("Tie!"); break;
+                case SCISSORS: computerScore++; console.log("You lose! Scissors beats Paper!"); break;
+            }
+            break;
+        case SCISSORS:
+            switch (computerChoice)
+            {
+                case ROCK:     computerScore++; console.log("You lose! Rock beats Scissors!"); break;
+                case PAPER:    humanScore++;    console.log("You win! Scissors beats Paper!"); break;
+                case SCISSORS:                  console.log("Tie!");
+            }
+            break;
+    }
+}
+
+const humanChoice     = getHumanChoice();
+const compueterChoice = getComputerChoice();
+
+playRound(humanChoice, compueterChoice);
