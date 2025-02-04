b9e877332d5467627c572c962ea3eafd0c83065b
gonrogon <gonrogon@gmail.com>
2025-02-04 13:13:28 +0100

    Show the score and the result of the last round

--- script.js
+++ script.js
@@ -35,34 +35,45 @@ function getHumanChoice()
 
 function playRound(humanChoice, computerChoice)
 {
+    let div = document.querySelector("#results");
+
     // Human choice validation and normalization (case-insensitive) is already done in
     // the function "getHumanChoice".
     switch (humanChoice) {
         case ROCK:
             switch (computerChoice)
             {
-                case ROCK:                      console.log("Tie!"); break;
-                case PAPER:    computerScore++; console.log("You lose! Paper beats Rock!"); break;
-                case SCISSORS: humanScore++;    console.log("You win! Rock beats Scissors!"); break;
+                case ROCK:                      addResult("Tie!"); break;
+                case PAPER:    computerScore++; addResult("You lose! Paper beats Rock!"); break;
+                case SCISSORS: humanScore++;    addResult("You win! Rock beats Scissors!"); break;
             }
             break;
         case PAPER:
             switch (computerChoice)
             {
-                case ROCK:     humanScore++;    console.log("You win! Paper beats Rock!"); break;
-                case PAPER:                     console.log("Tie!"); break;
-                case SCISSORS: computerScore++; console.log("You lose! Scissors beats Paper!"); break;
+                case ROCK:     humanScore++;    addResult("You win! Paper beats Rock!"); break;
+                case PAPER:                     addResult("Tie!"); break;
+                case SCISSORS: computerScore++; addResult("You lose! Scissors beats Paper!"); break;
             }
             break;
         case SCISSORS:
             switch (computerChoice)
             {
-                case ROCK:     computerScore++; console.log("You lose! Rock beats Scissors!"); break;
-                case PAPER:    humanScore++;    console.log("You win! Scissors beats Paper!"); break;
-                case SCISSORS:                  console.log("Tie!");
+                case ROCK:     computerScore++; addResult("You lose! Rock beats Scissors!"); break;
+                case PAPER:    humanScore++;    addResult("You win! Scissors beats Paper!"); break;
+                case SCISSORS:                  addResult("Tie!");
             }
             break;
     }
+}
+
+function addResult(text)
+{
+    let score  = document.querySelector("#score");
+    let result = document.querySelector("#results");
+    
+    score.textContent  = `Score: Human ${humanScore}, Computer ${computerScore}`;
+    result.textContent = text;
 }
 
 function setup() {
